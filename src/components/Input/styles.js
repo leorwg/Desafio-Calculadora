0bc8435c96682625e549b100557f672d13e19149
Leonardo Onofre <leorwg@gmail.com>
2022-12-16 22:37:48 -0300

    Desafio Calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #679cc9;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 30px;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #679cc9;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #ffffff;
+  }
+`;
